3dc77be15f9f737d260b26a206bdbd9bc87ecdf2
yangguang <yangguang@bilibili.com>
2017-12-04 19:53:28 +0800

    springboot init

---
+++ src/main/java/com/example/demo/Controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    public String say(){
+        return "Hello yangguang!Your fisrt springboot appliication is success";
+    }
+}
